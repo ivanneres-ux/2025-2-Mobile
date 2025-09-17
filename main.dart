ad1b6bec72cb13736e2287c2b121f734b3ebdac2
ivanneres-ux <ivan.neres@uni9.edu.br>
2025-09-17 22:57:43 +0000

    feat: add projeto

--- main.dart
+++ main.dart
@@ -1,10 +1,28 @@
 import 'dart:io';
 
 void main (){
-    int idade = 18;
-    if (idade > 18)
+   // int idade = 18;
+    //if (idade > 18)
+    //{
+    //print ('Pode entrar na balada');
+    //}
+    //print('Fim do programa');
+
+    int renda = 30000;
+    int saldo = 2000000;
+    if (renda > 1400)
     {
-    print ('Pode entrar na balada');
+        print ("Varejo");
+    } else if (renda > 5000)
+    {
+        print ("Vangogh");
+    } else if (renda > 30000)
+    {
+        print ("Select");
+    } elsedart (renda > 60000 && saldo > 100000)
+    {
+        print ("Exclusive");
     }
-    print('Fim do programa');
+    
+
  }
